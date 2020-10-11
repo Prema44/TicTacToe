25b8dcc0999897d7659497593b6c2f31dd16abe7
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-10-11 13:59:30 +0530

    Added : Functionality to Show the board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -38,6 +38,19 @@ public class TicTacToeGame {
 		}
 		return player;
 	}
+	
+	/**
+	 * Usecase 3
+	 * Function displays the board
+	 * 
+	 */
+	public static void showBoard() {
+		System.out.println(" | " + board[1] + " | " + " | " + board[2] + " | " + " | " + board[3] + " | ");
+		System.out.println("--------------------");
+		System.out.println(" | " + board[4] + " | " + " | " + board[5] + " | " + " | " + board[6] + " | ");
+		System.out.println("--------------------");
+		System.out.println(" | " + board[7] + " | " + " | " + board[8] + " | " + " | " + board[9] + " | ");	
+	}
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -49,6 +62,7 @@ public class TicTacToeGame {
 		else {
 			computer = 'X';
 		}
+		showBoard();
 	}
 }	
 
